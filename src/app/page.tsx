e8a25a76a7a5c8a1db52d7cedaf82b991ac69d16
Sreypoch <sreypoch.som@nbc.gov.kh>
2025-05-29 16:07:15 +0700

    add nbc logo to splash screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,14 +18,14 @@ export default function SplashScreen() {
 		<section className="min-h-screen flex flex-col justify-center items-center relative bg-[#1c3b63]">
 			{/* Centered Image */}
 			<img
-				src="/logo.png" // Replace this path with your actual image path
+				src="/images/nbc-logo.png" // Replace this path with your actual image path
 				alt="App Logo"
-				className="w-32 h-32 object-contain"
+				className="w-32 h-32 object-contain mb-[20px]"
 			/>
 			<h1 className="font-normal text-lg text-white">NBC Home</h1>
 
 			{/* Bottom Text */}
-			<div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 text-center px-4 text-white text-sm">
+			<div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 text-center px-4 text-white text-sm">
 				<span className="font-normal">Powered by:<br /></span>
 				<span className="font-medium">National Bank of Cambodia</span>
 			</div>
