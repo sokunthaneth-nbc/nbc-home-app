3a341f9888023bbaa900ee2681b8eaddf610903c
Soth Seyha <sseyha@sseyha-MacBook-Pro.local>
2025-06-04 13:56:29 +0700

    rollback next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
-    output: 'export',
-    trailingSlash: true,
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default withFlowbiteReact(nextConfig);
